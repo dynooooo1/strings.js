49d6da868d850c8bdc0ccf7f244bbf185365df41
dynooooo1 <dylandominno7@gmail.com>
2024-09-15 20:19:42 +0800

    Create strings.js

---
+++ strings.js
@@ -0,0 +1,17 @@
+let favActorFirstName = "Travis";
+let favActorLastName ="Fimmel";
+let fullName = favActorFirstName + " " + favActorLastName;
+
+let uppercase = fullName.toLowerCase();
+let lowercase = "Tokyo".toUpperCase();
+
+let message = "My favorite actor is" + " " + fullName.toLocaleUpperCase();
+
+message += ", his best show is Vikings.";
+
+console.log(favActorFirstName);
+console.log(favActorLastName);
+console.log(fullName);
+console.log(uppercase);
+console.log(lowercase);
+console.log(message);
